047ff8698641e5693b83d70298c77be25a5163d4
sachinrc69 <sachinrc04@gmail.com>
2024-09-19 00:18:36 +0530

    backend url update

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,2 @@
-export const url = "http://localhost:5000";
-// export const url =
-// "https://cafe69-t2h8iotnt-sachin-ratnu-chavans-projects.vercel.app/";
+// export const url = "http://localhost:5000";
+export const url = "https://cafe69.vercel.app//";
